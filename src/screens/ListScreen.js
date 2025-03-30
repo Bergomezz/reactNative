b8b47852d9b271180eabf89f4ed20f1eccadebf2
Bergomezz <bernardogomezz@yahoo.com.br>
2025-03-30 17:42:04 -0300

    adding age prop in the friends list

--- src/screens/ListScreen.js
+++ src/screens/ListScreen.js
@@ -3,15 +3,15 @@ import { Text, StyleSheet, View, FlatList } from 'react-native';
 
 const ListScreen = () => {
   const friends = [
-    { name: 'Friends #1' },
-    { name: 'Friends #2' },
-    { name: 'Friends #3' },
-    { name: 'Friends #4' },
-    { name: 'Friends #5' },
-    { name: 'Friends #6' },
-    { name: 'Friends #7' },
-    { name: 'Friends #8' },
-    { name: 'Friends #9' },
+    { name: 'Friends #1', age: 20 },
+    { name: 'Friends #2', age: 29 },
+    { name: 'Friends #3', age: 40 },
+    { name: 'Friends #4', age: 30 },
+    { name: 'Friends #5', age: 39 },
+    { name: 'Friends #6', age: 50 },
+    { name: 'Friends #7', age: 66 },
+    { name: 'Friends #8', age: 46 },
+    { name: 'Friends #9', age: 23 },
   ];
 
   return (
@@ -19,7 +19,11 @@ const ListScreen = () => {
       keyExtractor={friend => friend.name}
       data={friends}
       renderItem={({ item }) => {
-        return <Text style={styles.textStyle}>{item.name}</Text>;
+        return (
+          <Text style={styles.textStyle}>
+            {item.name} - Age: {item.age}
+          </Text>
+        );
       }}
     />
   );
