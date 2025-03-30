d421b3c94ca6db249b335d0a36f2a51de1c2851f
Bergomezz <bernardogomezz@yahoo.com.br>
2025-03-30 17:13:29 -0300

    ended componentsScreen and listScreen

---
+++ src/screens/ListScreen.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import { Text, StyleSheet, View, FlatList } from 'react-native';
+
+const ListScreen = () => {
+  const friends = [
+    { name: 'Friends #1' },
+    { name: 'Friends #2' },
+    { name: 'Friends #3' },
+    { name: 'Friends #4' },
+    { name: 'Friends #5' },
+    { name: 'Friends #6' },
+    { name: 'Friends #7' },
+    { name: 'Friends #8' },
+    { name: 'Friends #9' },
+  ];
+
+  return (
+    <FlatList
+      keyExtractor={friend => friend.name}
+      data={friends}
+      renderItem={({ item }) => {
+        return <Text style={styles.textStyle}>{item.name}</Text>;
+      }}
+    />
+  );
+};
+
+const styles = StyleSheet.create({
+  textStyle: {
+    marginVertical: 50,
+  },
+});
+
+export default ListScreen;
